2ff97234c3a5f5ebc64607de59cba6c669df9435
JeremYD <delpech.jeremy@hotmail.fr>
2020-08-28 16:11:32 +0200

    Update scripts

--- docker-script.sh
+++ docker-script.sh
@@ -1,5 +1,5 @@
 echo "### Génération de l'image postresql-mission-db ###"
-docker build -t postresql-mission-db .
+docker build -t postgresql-mission-db .
 
 echo "### Création du containeur postgresql ###"
 docker run -d --name postgresql-mission-db postgresql-mission-db 
