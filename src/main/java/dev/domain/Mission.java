6547aa3100ebb4c976cb576b8c00fbb915a29e10
Antoine LABEEUW <antoine.labeeuw@gmail.com>
2020-08-31 11:06:29 +0200

    Ajout d'un mapping JPA entre Mission et Nature

---
+++ src/main/java/dev/domain/Mission.java
@@ -0,0 +1,147 @@
+package dev.domain;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+/**
+ * classe conceptualisant une mission
+ * 
+ * @author antoinelabeeuw
+ *
+ */
+@Entity
+public class Mission extends EntiteModifiable {
+	/** dateDebut */
+	private LocalDate dateDebut;
+	/** dateFin */
+	private LocalDate dateFin;
+	/** villeDepart */
+	private String villeDepart;
+	/** villeArrivee */
+	private String villeArrivee;
+	/** prime */
+	private BigDecimal prime;
+	/** nature */
+	@ManyToOne
+	@JoinColumn(name = "nature_id")
+	private Nature nature;
+
+	/**
+	 * Constructeur
+	 * 
+	 * @param signatureNumerique
+	 */
+	public Mission(SignatureNumerique signatureNumerique) {
+		super(signatureNumerique);
+	}
+
+	/**
+	 * Getter
+	 * 
+	 * @return the dateDebut
+	 */
+	public LocalDate getDateDebut() {
+		return dateDebut;
+	}
+
+	/**
+	 * Setter
+	 * 
+	 * @param dateDebut the dateDebut to set
+	 */
+	public void setDateDebut(LocalDate dateDebut) {
+		this.dateDebut = dateDebut;
+	}
+
+	/**
+	 * Getter
+	 * 
+	 * @return the dateFin
+	 */
+	public LocalDate getDateFin() {
+		return dateFin;
+	}
+
+	/**
+	 * Setter
+	 * 
+	 * @param dateFin the dateFin to set
+	 */
+	public void setDateFin(LocalDate dateFin) {
+		this.dateFin = dateFin;
+	}
+
+	/**
+	 * Getter
+	 * 
+	 * @return the villeDepart
+	 */
+	public String getVilleDepart() {
+		return villeDepart;
+	}
+
+	/**
+	 * Setter
+	 * 
+	 * @param villeDepart the villeDepart to set
+	 */
+	public void setVilleDepart(String villeDepart) {
+		this.villeDepart = villeDepart;
+	}
+
+	/**
+	 * Getter
+	 * 
+	 * @return the villeArrivee
+	 */
+	public String getVilleArrivee() {
+		return villeArrivee;
+	}
+
+	/**
+	 * Setter
+	 * 
+	 * @param villeArrivee the villeArrivee to set
+	 */
+	public void setVilleArrivee(String villeArrivee) {
+		this.villeArrivee = villeArrivee;
+	}
+
+	/**
+	 * Getter
+	 * 
+	 * @return the prime
+	 */
+	public BigDecimal getPrime() {
+		return prime;
+	}
+
+	/**
+	 * Setter
+	 * 
+	 * @param prime the prime to set
+	 */
+	public void setPrime(BigDecimal prime) {
+		this.prime = prime;
+	}
+
+	/** Getter
+	 * @return the nature
+	 */
+	public Nature getNature() {
+		return nature;
+	}
+
+	/** Setter
+	 * @param nature the nature to set
+	 */
+	public void setNature(Nature nature) {
+		this.nature = nature;
+	}
+	
+
+}
--- src/main/java/dev/domain/Nature.java
+++ src/main/java/dev/domain/Nature.java
@@ -2,9 +2,14 @@ package dev.domain;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 
 import javax.persistence.Entity;
-/** classe conceptualisant une nature
+import javax.persistence.OneToMany;
+
+/**
+ * classe conceptualisant une nature
+ * 
  * @author antoinelabeeuw
  *
  */
@@ -24,107 +29,153 @@ public class Nature extends EntiteModifiable {
 	private BigDecimal plafondFrais;
 	/** depassementFrais */
 	private BigDecimal depassementFrais;
-	
+	/** missions */
+	@OneToMany(mappedBy = "nature")
+	private List<Mission> missions;
+
 	public Nature(SignatureNumerique signatureNumerique) {
 		super(signatureNumerique);
 	}
 
-	/** Getter
+	/**
+	 * Getter
+	 * 
 	 * @return the payee
 	 */
 	public Boolean getPayee() {
 		return payee;
 	}
 
-	/** Setter
+	/**
+	 * Setter
+	 * 
 	 * @param payee the payee to set
 	 */
 	public void setPayee(Boolean payee) {
 		this.payee = payee;
 	}
 
-	/** Getter
+	/**
+	 * Getter
+	 * 
 	 * @return the versementPrime
 	 */
 	public Boolean getVersementPrime() {
 		return versementPrime;
 	}
 
-	/** Setter
+	/**
+	 * Setter
+	 * 
 	 * @param versementPrime the versementPrime to set
 	 */
 	public void setVersementPrime(Boolean versementPrime) {
 		this.versementPrime = versementPrime;
 	}
 
-	/** Getter
+	/**
+	 * Getter
+	 * 
 	 * @return the pourcentagePrime
 	 */
 	public BigDecimal getPourcentagePrime() {
 		return pourcentagePrime;
 	}
 
-	/** Setter
+	/**
+	 * Setter
+	 * 
 	 * @param pourcentagePrime the pourcentagePrime to set
 	 */
 	public void setPourcentagePrime(BigDecimal pourcentagePrime) {
 		this.pourcentagePrime = pourcentagePrime;
 	}
 
-	/** Getter
+	/**
+	 * Getter
+	 * 
 	 * @return the debutValidite
 	 */
 	public LocalDate getDebutValidite() {
 		return debutValidite;
 	}
 
-	/** Setter
+	/**
+	 * Setter
+	 * 
 	 * @param debutValidite the debutValidite to set
 	 */
 	public void setDebutValidite(LocalDate debutValidite) {
 		this.debutValidite = debutValidite;
 	}
 
-	/** Getter
+	/**
+	 * Getter
+	 * 
 	 * @return the finValidite
 	 */
 	public LocalDate getFinValidite() {
 		return finValidite;
 	}
 
-	/** Setter
+	/**
+	 * Setter
+	 * 
 	 * @param finValidite the finValidite to set
 	 */
 	public void setFinValidite(LocalDate finValidite) {
 		this.finValidite = finValidite;
 	}
 
-	/** Getter
+	/**
+	 * Getter
+	 * 
 	 * @return the plafondFrais
 	 */
 	public BigDecimal getPlafondFrais() {
 		return plafondFrais;
 	}
 
-	/** Setter
+	/**
+	 * Setter
+	 * 
 	 * @param plafondFrais the plafondFrais to set
 	 */
 	public void setPlafondFrais(BigDecimal plafondFrais) {
 		this.plafondFrais = plafondFrais;
 	}
 
-	/** Getter
+	/**
+	 * Getter
+	 * 
 	 * @return the depassementFrais
 	 */
 	public BigDecimal getDepassementFrais() {
 		return depassementFrais;
 	}
 
-	/** Setter
+	/**
+	 * Setter
+	 * 
 	 * @param depassementFrais the depassementFrais to set
 	 */
 	public void setDepassementFrais(BigDecimal depassementFrais) {
 		this.depassementFrais = depassementFrais;
 	}
+
+	/** Getter
+	 * @return the missions
+	 */
+	public List<Mission> getMissions() {
+		return missions;
+	}
+
+	/** Setter
+	 * @param missions the missions to set
+	 */
+	public void setMissions(List<Mission> missions) {
+		this.missions = missions;
+	}
 	
+
 }
