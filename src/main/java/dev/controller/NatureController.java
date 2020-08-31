e2e4731355c0f174aedacd43e6b2aeea63e30ddb
Léo SENIQUE <leo.senique@gmail.com>
2020-08-31 17:39:36 +0200

    ajout nature controller et service

--- src/main/java/dev/controller/NatureController.java
+++ src/main/java/dev/controller/NatureController.java
@@ -4,5 +4,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class NatureController {
+	
+	private NatureService service;
+	
+	public NatureController(NatureService service) {
+		this.service = service;
+	}
+	
+	
 
 }
