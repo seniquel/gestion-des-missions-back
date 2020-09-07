9f71da9a44c8c264d5a4147538305a13bec10e99
JeremYD <delpech.jeremy@hotmail.fr>
2020-09-07 15:50:19 +0200

    USGDM030 - statut mission modifié #22

--- src/main/java/dev/excel/ExportMissions.java
+++ src/main/java/dev/excel/ExportMissions.java
@@ -17,90 +17,93 @@ import org.apache.poi.ss.usermodel.Row;
 
 import dev.domain.Mission;
 
-/**Export d'une liste de missions en fichier xsl
+/**
+ * Export d'une liste de missions en fichier xsl
+ * 
  * @author formation
  *
  */
 public interface ExportMissions {
 
 	private static HSSFCellStyle createStyleForTitle(HSSFWorkbook workbook) {
-        HSSFFont font = workbook.createFont();
-        font.setBold(true);
-        HSSFCellStyle style = workbook.createCellStyle();
-        style.setFont(font);
-        return style;
-    }
-	
+		HSSFFont font = workbook.createFont();
+		font.setBold(true);
+		HSSFCellStyle style = workbook.createCellStyle();
+		style.setFont(font);
+		return style;
+	}
+
 	public static void creerFichierExcel(List<Mission> list, int annee) {
-		
+
 		HSSFWorkbook workbook = new HSSFWorkbook();
-        HSSFSheet sheet = workbook.createSheet("Employees sheet");
- 
-        int rownum = 0;
-        Cell cell;
-        Row row;
-        //
-        HSSFCellStyle style = createStyleForTitle(workbook);
- 
-        row = sheet.createRow(rownum);
- 
-        // Début
-        cell = row.createCell(0, CellType.STRING);
-        cell.setCellValue("Date de début");
-        cell.setCellStyle(style);
-        // Fin
-        cell = row.createCell(1, CellType.STRING);
-        cell.setCellValue("Date de fin");
-        cell.setCellStyle(style);
-        // Nature
-        cell = row.createCell(2, CellType.STRING);
-        cell.setCellValue("Nature");
-        cell.setCellStyle(style);
-        // Prime
-        cell = row.createCell(3, CellType.STRING);
-        cell.setCellValue("Prime");
-        cell.setCellStyle(style);
-        
-     // Data
-        for (Mission mission : list) {
-            rownum++;
-            row = sheet.createRow(rownum);
- 
-            // Début
-            cell = row.createCell(0, CellType.STRING);
-            cell.setCellValue(mission.getDateDebut().toString());
-            // Fin)
-            cell = row.createCell(1, CellType.STRING);
-            cell.setCellValue(mission.getDateFin().toString());
-            // Nature
-            cell = row.createCell(2, CellType.NUMERIC);
-            cell.setCellValue(mission.getNature().getLibelle());
-            // Prime)
-            cell = row.createCell(3, CellType.STRING);
-            cell.setCellValue(mission.getPrime().toString()+"€");
-        }
-        
-        FileOutputStream fop = null;
-        File file;
-        
-        try {
-        	file = new File("C:/Users/"+System.getProperty("user.name")+"/primes/"+LocalDate.now()+"-primes-"+annee+".xls");
-            file.getParentFile().mkdirs();
-     
-            fop = new FileOutputStream(file);
-            workbook.write(fop);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (fop != null) {
-                    fop.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        
+		HSSFSheet sheet = workbook.createSheet("Employees sheet");
+
+		int rownum = 0;
+		Cell cell;
+		Row row;
+		//
+		HSSFCellStyle style = createStyleForTitle(workbook);
+
+		row = sheet.createRow(rownum);
+
+		// Début
+		cell = row.createCell(0, CellType.STRING);
+		cell.setCellValue("Date de début");
+		cell.setCellStyle(style);
+		// Fin
+		cell = row.createCell(1, CellType.STRING);
+		cell.setCellValue("Date de fin");
+		cell.setCellStyle(style);
+		// Nature
+		cell = row.createCell(2, CellType.STRING);
+		cell.setCellValue("Nature");
+		cell.setCellStyle(style);
+		// Prime
+		cell = row.createCell(3, CellType.STRING);
+		cell.setCellValue("Prime");
+		cell.setCellStyle(style);
+
+		// Data
+		for (Mission mission : list) {
+			rownum++;
+			row = sheet.createRow(rownum);
+
+			// Début
+			cell = row.createCell(0, CellType.STRING);
+			cell.setCellValue(mission.getDateDebut().toString());
+			// Fin)
+			cell = row.createCell(1, CellType.STRING);
+			cell.setCellValue(mission.getDateFin().toString());
+			// Nature
+			cell = row.createCell(2, CellType.NUMERIC);
+			cell.setCellValue(mission.getNature().getLibelle());
+			// Prime)
+			cell = row.createCell(3, CellType.STRING);
+			cell.setCellValue(mission.getPrime().toString() + "€");
+		}
+
+		FileOutputStream fop = null;
+		File file;
+
+		try {
+			file = new File("C:/Users/" + System.getProperty("user.name") + "/primes/" + LocalDate.now() + "-primes-"
+					+ annee + ".xls");
+			file.getParentFile().mkdirs();
+
+			fop = new FileOutputStream(file);
+			workbook.write(fop);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (fop != null) {
+					fop.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
 	}
 
 }
